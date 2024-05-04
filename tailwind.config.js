5b30337ae7cc833196b15ed265ff79bf76db220e
Jean Gustave NSABIYERA <jeangustave.nsabiyera@arhs-cube.com>
2024-05-04 21:51:50 +0200

    enable dark mode switching

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
         twitter: "hsl(203, 89%, 53%)",
         youtube: "hsl(348, 97%, 39%)",
         "dark-blue": "hsl(230, 17%, 14%)",
-        "very-dark-blue": "hsl(232, 19%, 15%)",
+        "very-dark-blue": "hsl(230, 17%, 14%)",
+        "very-dark-blue-bg": "hsl(232, 19%, 15%)",
         "dark-desaturated-blue": "hsl(228, 28%, 20%)",
         "dark-desaturated-blue-hover": "hsl(228, 24%, 39%)",
         "desaturate-blue": " hsl(228, 34%, 66%)",
@@ -21,6 +22,8 @@ export default {
         "light-grayish-blue": "hsl(227, 47%, 96%)",
         "light-grayish-blue-hover": "hsl(227, 6%, 84%)",
         "text-dark-blue": "hsl(230, 17%, 14%)",
+        "bg-toggle-track": "hsl(232 , 23%, 74%)",
+
       },
       backgroundImage: {
         "gradient-instagram": "linear-gradient(90deg, hsl(37, 97%, 70%),hsl(329, 70%, 58%))",
